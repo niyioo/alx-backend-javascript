a54104af4a916f80acdb4e4aa8e67c186e734851
niyioo <niyiolagbegi@gmail.com>
2024-01-03 01:03:49 -0800

    fix task 0 and 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,8 @@
-// Use const for taskFirst function
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Use let for taskNext function
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
@@ -12,7 +10,6 @@ export function taskNext() {
   return combination;
 }
 
-// getLast function remains the same
 export function getLast() {
   return ' is okay';
 }
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  // Use let and const instead of var
   let task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    // Use let and const instead of var
     let task = true;
     const task2 = false;
   }
