01120a72897b701381bba67a47de060624e2af50
niyioo <niyiolagbegi@gmail.com>
2024-02-26 13:49:05 +0100

    fixed

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import routes from './routes';
+const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 1245;
@@ -10,4 +10,4 @@ app.listen(PORT, () => {
     console.log(`Server is running and listening on port ${PORT}`);
 });
 
-export default app;
+module.exports = app;
