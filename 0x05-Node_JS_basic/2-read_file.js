2de1552cc14dcb1ec5ee2fd1e32b59eca121e522
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 15:44:37 +0100

    v

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,33 +2,26 @@ const fs = require('fs');
 
 /**
  * Counts the number of students in each field from the database file.
- * @param {string} path - The path to the database file.
+ * @param {string} filepath - The path to the database file.
  */
-function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8');
 
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-
-        const fieldCounts = {};
-
-        lines.forEach(line => {
-            const [firstName, , field] = line.split(',');
-            if (fieldCounts[field]) {
-                fieldCounts[field].push(firstName);
-            } else {
-                fieldCounts[field] = [firstName];
-            }
-        });
-
-        console.log(`Number of students: ${lines.length}`);
-
-        for (const field in fieldCounts) {
-            console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+const countStudents = (filePath) => {
+  try {
+    let data = fs.readFileSync(filePath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-}
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 module.exports = countStudents;
