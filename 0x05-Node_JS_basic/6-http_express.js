38454bfe93f2f81b7a10806d6ac1e847725fc0ae
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 15:52:27 +0100

    c

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,18 +1,6 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
-});
-
-app.use((req, res) => {
-    res.status(404).send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ' + req.url + '</pre></body></html>');
-});
-
-const PORT = 1245;
-app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
-});
-
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.listen(1245);
 module.exports = app;
