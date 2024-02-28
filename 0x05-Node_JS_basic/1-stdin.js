deb247596a6449ddddd09489ab4f3439901d11ef
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 14:43:41 +0100

    cc

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Listen for user input
 process.stdin.on('readable', () => {
   // Extract the input (trim to remove leading/trailing whitespace)
-  const input = process.stdin.read();
-  if (input !== null) {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
     // Display the user's name
     process.stdout.write(`Your name is: ${input}\n`);
   }
