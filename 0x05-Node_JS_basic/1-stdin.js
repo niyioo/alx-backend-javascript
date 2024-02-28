6474512b5eac12181da401fda7429401f7adf988
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 14:40:35 +0100

    bbb

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
-// Display the initial message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input
-process.stdin.on('data', (data) => {
+process.stdin.on('readable', () => {
   // Extract the input (trim to remove leading/trailing whitespace)
-  const input = data.toString().trim();
+  const input = process.stdin.read();
+  if (input !== null) {
+    // Display the user's name
+    process.stdout.write(`Your name is: ${input}\n`);
+  }
+});
 
-  // Display the user's name
-  process.stdout.write(`Your name is: ${input}\n`);
-
+process.stdin.on('end', () => {
   // Notify that the program is closing
   process.stdout.write('This important software is now closing\n');
-
-  // Exit the process
-  process.exit();
 });
+
