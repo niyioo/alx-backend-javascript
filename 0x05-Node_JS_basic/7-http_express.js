9b23d722d8167bf6c4878c32c6780c271c90eb2d
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 15:53:27 +0100

    fx

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,48 +1,16 @@
 const express = require('express');
-const { promisify } = require('util');
-const fs = require('fs');
-
-const readFileAsync = promisify(fs.readFile);
+const fetchStudentCount = require('./3-read_file_async');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+app.get('/', (request, response) => response.send('Hello Holberton School!'));
+app.get('/students', async (request, response) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await fetchStudentCount(process.argv[2]);
+    response.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    response.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/students', async (req, res) => {
-    try {
-        const data = await readFileAsync(process.argv[2], 'utf8');
-
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-
-        const fieldCounts = {};
-
-        lines.forEach(line => {
-            const [, , field] = line.split(',');
-            if (fieldCounts[field]) {
-                fieldCounts[field]++;
-            } else {
-                fieldCounts[field] = 1;
-            }
-        });
-
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${lines.length}\n`);
-
-        for (const field in fieldCounts) {
-            res.write(`Number of students in ${field}: ${fieldCounts[field]}\n`);
-        }
-
-        res.end();
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-const PORT = 1245;
-app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
-});
-
+app.listen(1245);
 module.exports = app;
