315325a833f5e86a4114295b2c8d6b7e9db5a4af
niyioo <adeniyi@cybersecinsight.com>
2024-02-28 15:25:04 +0100

    b

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdin.on('data', (data) => {
 
   process.stdout.write(`Your name is: ${input}\n`);
 
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 
   process.exit();
 });
