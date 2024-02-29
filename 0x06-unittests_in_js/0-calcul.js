e3240d1efd7b86f57d27ffdf59c447afe20551b2
niyioo <adeniyi@cybersecinsight.com>
2024-02-29 12:56:45 +0100

    fe

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -7,7 +7,7 @@
  * @returns {number} The sum of the rounded numbers.
  */
 function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b);
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
