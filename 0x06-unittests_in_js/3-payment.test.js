b7efec02161695c731f4c9b9f16721fb9095cbe6
niyioo <adeniyi@cybersecinsight.com>
2024-02-29 14:12:53 +0100

    fx

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,23 @@
-const { describe, it } = require('mocha');
 const sinon = require('sinon');
-const assert = require('assert');
-const sendPaymentRequestToApi = require('./3-payment');
+const { describe, it } = require('mocha');
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+// Import describe and it from Mocha
+
+// Import assert if you are using it
+// const assert = require('assert');
 
 describe('sendPaymentRequestToApi', () => {
-  it('check that Utils.calculateNumber was called once', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const totalAmount = 100;
+    const totalShipping = 20;
 
-    sendPaymentRequestToApi(50, 24.52);
+    sendPaymentRequestToApi(totalAmount, totalShipping);
 
-    assert(spy.calledOnce);
-    spy.restore();
+    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', totalAmount, totalShipping);
+
+    calculateNumberSpy.restore();
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,26 @@
-const { describe, it } = require('mocha');
 const sinon = require('sinon');
-const assert = require('assert');
-const sendPaymentRequestToApi = require('./4-payment');
+const { describe, it } = require('mocha');
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+// Import describe and it from Mocha
+
+// Remove unused assert import
+// const assert = require('assert');
 
 describe('sendPaymentRequestToApi', () => {
-  it('check that Utils.calculateNumber is stubbed', () => {
-    const spy = sinon.spy(console, 'log');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    sendPaymentRequestToApi(100, 20);
+  it('should call Utils.calculateNumber with correct arguments and log correct message', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    const totalAmount = 100;
+    const totalShipping = 20;
 
-    assert(spy.withArgs('The total is: 10').calledOnce);
-    assert(stub.withArgs('SUM', 100, 20).calledOnce);
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', totalAmount, totalShipping);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 10');
+
+    calculateNumberStub.restore();
+    consoleSpy.restore();
   });
 });
