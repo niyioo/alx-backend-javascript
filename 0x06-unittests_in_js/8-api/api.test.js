91a84530f0705aa93ae5a17962f46dfdb9393f69
niyioo <adeniyi@cybersecinsight.com>
2024-02-29 14:36:07 +0100

    task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('should return correct status code', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  // Add other tests as needed
+});
