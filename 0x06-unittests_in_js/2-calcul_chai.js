027c77a61aa56d321962085f0d7c665142f54436
niyioo <adeniyi@cybersecinsight.com>
2024-02-29 13:45:46 +0100

    task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,16 @@
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } if (type === 'DIVIDE') {
+    const roundedB = Math.round(b);
+    if (roundedB === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / roundedB;
+  }
+  throw new Error('Invalid type');
+}
+
+module.exports = calculateNumber;
