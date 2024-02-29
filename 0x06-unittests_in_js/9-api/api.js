0768403692ce4d32a1ea5fa0ad93e4e9c73e115e
niyioo <adeniyi@cybersecinsight.com>
2024-02-29 14:58:19 +0100

    fxe

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
 const app = express();
-const PORT = 7865;
+const port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send('Payment methods for cart ${request.params.id}');
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
 });
-
-module.exports = app;
